0bae06a269b33bd7cac4f4cb4f0f905a157f03ba
Viet Hoang <viethoangquoc2711@gmail.com>
2019-01-21 19:35:48 +0700

    - Add Media Url

--- hsc_backend/users/urls.py
+++ hsc_backend/users/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import routers
 from django.conf.urls import url, include, re_path
 from .views import UserViewSet
@@ -10,3 +12,5 @@ app_name = "users"
 urlpatterns = [
     re_path(r'^', include(router.urls))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
